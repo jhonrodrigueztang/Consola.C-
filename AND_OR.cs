a85b4991a9a9a67d86d8209f4f91feadba063397
jhonrodrigueztang <jhonrodrigueztang@gmail.com>
2026-03-12 20:46:31 -0600

    Console.AND_OR
    programa donde la consola muestra el programa con las sentencias AND && y OR || con FN sale las barritas

---
+++ AND_OR.cs
@@ -0,0 +1,17 @@
+﻿int NUMERO1 = 40, NUMERO2 = 90 , numero3 = 5;
+Boolean resultado;
+
+//Operador AND
+resultado = NUMERO1 < NUMERO2 && NUMERO1 < numero3 ;
+
+Console.WriteLine(resultado);
+
+//Operador OR 
+
+resultado = NUMERO1 < NUMERO2 || NUMERO1 < numero3;
+
+Console.WriteLine(resultado);
+
+
+//Readline hace que desaparezca lo extras de consola por ahora.
+Console.ReadLine();
